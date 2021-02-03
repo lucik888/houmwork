c1543d484ffe086dc034d052bac312bf3f0daad5
lucik888 <77534718+lucik888@users.noreply.github.com>
2021-02-03 16:00:28 +0300

    Лис накодил

---
+++ hello.js
@@ -0,0 +1,8 @@
+const hello = (fn, ln) => {
+     return `Привет, ${fn} ${ln}!`;
+}
+
+alert(hello("Dmytry","Karpov"));
+
+// // const hello = (fn, ln) => `Привет, ${fn} ${ln}!
+// alert(hello("Dmytry","Karpov"));
