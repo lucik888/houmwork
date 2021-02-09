fbeb1a5a7e4a7dcadc3a25643017a64ce771511a
lucik888 <77534718+lucik888@users.noreply.github.com>
2021-02-09 14:39:18 +0300

    Добавил квадрат

--- addEventListener.js
+++ addEventListener.js
@@ -19,3 +19,25 @@ square.addEventListener('click', () => {
     square.style.transform = 'rotate(180deg)';
   }
 });
+
+const square2 = document.querySelector('.square2');
+
+square2.addEventListener('click', () => {
+  const red = Math.round(Math.random() * 255);
+  const green = Math.round(Math.random() * 255);
+  const blue = Math.round(Math.random() * 255);
+
+  square2.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+
+  if (square2.style.borderRadius === `50%`) {
+    square2.style.borderRadius = `0`;
+  }
+  else {
+    square2.style.borderRadius = `20%`
+  }
+  if (square2.style.transform === 'rotate(180deg)') {
+    square2.style.transform = '';
+  } else {
+    square2.style.transform = 'rotate(180deg)';
+  }
+   });
