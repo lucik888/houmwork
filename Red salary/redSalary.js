3011a9d987e36ecafb357645588a152074cff144
lucik888 <77534718+lucik888@users.noreply.github.com>
2021-02-18 16:00:55 +0300

    Не получается

--- Red salary/redSalary.js
+++ Red salary/redSalary.js
@@ -1,27 +1,33 @@
 const fee = (md, rd, tax) => {
+    if (undefined, undefined, undefined) {
+        return "заполните форму";
+    }
     return rd * (tax / md);
 }
 
-// ищем элемент формы по id "form"
 const form = document.getElementById('form');
-// ищем элемент результа по id "result"
 const result = document.getElementById('result');
 
-// добавляем обработчик на событие "submit" - отправка формы
 form.addEventListener('submit', (ev) => {
-    // предотвращаем перезагрузку страницы (стандартное поведение)
+
     ev.preventDefault();
-    // обращаемся к элементам формы по их id
-    const { md, rd, tax } = ev.target.elements;
+    if (fee(undefined, undefined, undefined)) {
+        result.textContent = "заполните форму";
+    }
+      const { md, rd, tax } = ev.target.elements;
+        result.textContent = fee(md.value, rd.value, tax.value);
 
-    // записываем результат работы функции fee в элемент result (вывод результата на экран)
-    result.textContent = fee(md.value, rd.value, tax.value);
-
-    // TODO: сделать так, чтобы выводилась ошибка, когда значения не введены в поля (сейчас выдает NaN)
 });
 
-// добавляем обработчик на событие "reset" - сброс формы
 form.addEventListener('reset', () => {
-    // отчищаем содержимое элемента result
+
     result.textContent = '';
 });
+
+const makeNegative = (num) => {
+    if (num <= 0){
+        return num;
+    }
+    return -num;
+}
+console.log(makeNegative(5));
