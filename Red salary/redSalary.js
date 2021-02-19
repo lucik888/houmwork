d4364ce59c0f46afe98586fad7c0119a66c289b2
lucik888 <77534718+lucik888@users.noreply.github.com>
2021-02-19 12:52:25 +0300

    Получилось))

--- Red salary/redSalary.js
+++ Red salary/redSalary.js
@@ -1,8 +1,5 @@
 const fee = (md, rd, tax) => {
-    if (undefined, undefined, undefined) {
-        return "заполните форму";
-    }
-    return rd * (tax / md);
+          return rd * (tax / md);
 }
 
 const form = document.getElementById('form');
@@ -10,12 +7,11 @@ const result = document.getElementById('result');
 
 form.addEventListener('submit', (ev) => {
 
+
     ev.preventDefault();
-    if (fee(undefined , undefined, undefined)) {
-        result.textContent = "заполните форму";
-    }
-      const { md, rd, tax } = ev.target.elements;
-        result.textContent = fee(md.value, rd.value, tax.value);
+    const { md, rd, tax } = ev.target.elements;
+
+    result.textContent = fee(md.value, rd.value, tax.value);
 
 });
 
