1440b28ad619a647328f66a1208090c9a459b2ca
Andrew <Andrew5282011@gmail.com>
2021-02-27 18:29:24 +0300

    Починены стили, добавлен JS. Задание на дом: 1. Отчищать поле ввода после клика по кнопке Add todo 2. Реализовать обработку клика по кнопке Remove (удаление элементов из списка) 3. Реализовать переключение data-done по клику на элемент списка

---
+++ todoApp/Alex TODO/Alex TODO.js
@@ -0,0 +1,75 @@
+const input = document.getElementById('todo-input');
+const form = document.getElementById('todo-form');
+const list = document.getElementById('todo-list');
+
+const createNewTodo = (text) => {
+  // создаем элемент span
+  const label = document.createElement('span');
+  label.classList.add('pointer', 'list-group-item-content');
+  label.dataset.done = 'false';
+  label.textContent = text;
+
+  // создаем элемент button
+  const removeButton = document.createElement('button');
+  removeButton.classList.add('btn-remove', 'btn', 'btn-sm', 'btn-primary');
+  removeButton.textContent = 'Remove';
+
+  // создаем элемент li
+  const listItem = document.createElement('li');
+  listItem.classList.add('list-group-item');
+
+  // добавляем элементы span и button внутрь li
+  listItem.appendChild(label);
+  listItem.appendChild(removeButton);
+
+  return listItem;
+};
+
+form.addEventListener('submit', (ev) => {
+  // предотвратить действие по-умолчанию (для события submit - это перезагрузка страницы)
+  ev.preventDefault();
+  // извлекаем значение из input (текст)
+  const currentValue = input.value;
+  // создаем новый элемент TODO
+  const newTodoItem = createNewTodo(currentValue);
+  // добавляем созданный элемент внутрь списка
+  list.appendChild(newTodoItem);
+});
+
+// <li className="list-group-item">
+//   <span className="pointer list-group-item-content" data-done="true"> Some todo item </span>
+//   <button className="btn-remove btn btn-sm btn-primary"> Remove</button>
+// </li>
+
+// const TodoListItem = (props) => {
+//   const { label, onRemove } = props;
+//
+//   const [isDone, setIsDone] = React.useState(false);
+//
+//   return (
+//     <li className="list-group-item" onClick={() => setIsDone(!isDone)}>
+//       <span className="pointer list-group-item-content" data-done={isDone}>
+//         {label}
+//       </span>
+//       <button onClick={onRemove} className="btn-remove btn btn-sm btn-primary">Remove</button>
+//     </li>
+//   )
+// };
+
+// const TodoList = (props) => {
+//   const { items, onRemove } = props;
+//
+//   return (
+//     <ul>
+//       {items.map((item) => (
+//         <TodoListItem
+//           key={item.id}
+//           label={item.text}
+//           onRemove={onRemove}
+//         />
+//       ))}
+//     </ul>
+//   );
+// }
+
+// React.createElement('li', { className: 'list-group-item' })
