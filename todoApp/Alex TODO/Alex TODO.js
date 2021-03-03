e0aef1cd1b843849a1cedf3153a47ea50e817d4a
lucik888 <77534718+lucik888@users.noreply.github.com>
2021-03-03 14:57:32 +0300

    Ничего не работает в кансоли

--- todoApp/Alex TODO/Alex TODO.js
+++ todoApp/Alex TODO/Alex TODO.js
@@ -1,40 +1,74 @@
-const input = document.getElementById('todo-input');
-const form = document.getElementById('todo-form');
-const list = document.getElementById('todo-list');
+const input = document.querySelector('#todo-list'),
+  addButton = document.querySelector('#add-button'),
+  todo = document.querySelector('#todo-list')
 
-const createNewTodo = (text) => {
-  // создаем элемент span
-  const label = document.createElement('span');
-  label.classList.add('pointer', 'list-group-item-content');
-  label.dataset.done = 'false';
-  label.textContent = text;
+const todoList = [];
 
-  // создаем элемент button
-  const removeButton = document.createElement('button');
-  removeButton.classList.add('btn-remove', 'btn', 'btn-sm', 'btn-primary');
-  removeButton.textContent = 'Remove';
+addButton.addEventListener('click',function() {
 
-  // создаем элемент li
-  const listItem = document.createElement('li');
-  listItem.classList.add('list-group-item');
-
-  // добавляем элементы span и button внутрь li
-  listItem.appendChild(label);
-  listItem.appendChild(removeButton);
-
-  return listItem;
-};
-
-form.addEventListener('submit', (ev) => {
-  // предотвратить действие по-умолчанию (для события submit - это перезагрузка страницы)
-  ev.preventDefault();
-  // извлекаем значение из input (текст)
-  const currentValue = input.value;
-  // создаем новый элемент TODO
-  const newTodoItem = createNewTodo(currentValue);
-  // добавляем созданный элемент внутрь списка
-  list.appendChild(newTodoItem);
+  let newTodo = {
+    todo: input.value,
+    checked: false,
+    important: false
+  };
+  todoList.push(newTodo);
 });
+
+function displayMessages (){
+  todoList.forEach(function(item, i){
+    let displayMessages = `наше дело: ${input.value}`;
+  });
+}
+
+
+
+
+// const input = document.getElementById('todo-input');
+// const form = document.getElementById('todo-form');
+// const list = document.getElementById('todo-list');
+//
+// const createNewTodo = (text) => {
+//   // создаем элемент span
+//   const label = document.createElement('span');
+//   label.classList.add('pointer', 'list-group-item-content');
+//   label.dataset.done = 'false';
+//   label.textContent = text;
+//
+//   // создаем элемент button
+//   const removeButton = document.createElement('button');
+//   removeButton.classList.add('btn-remove', 'btn', 'btn-sm', 'btn-primary');
+//   removeButton.textContent = 'Remove';
+//
+//   // создаем элемент li
+//   const listItem = document.createElement('li');
+//   listItem.classList.add('list-group-item');
+//
+//   // добавляем элементы span и button внутрь li
+//   listItem.appendChild(label);
+//   listItem.appendChild(removeButton);
+//
+//   return listItem;
+// };
+//
+// form.addEventListener('submit', (ev) => {
+//   // предотвратить действие по-умолчанию (для события submit - это перезагрузка страницы)
+//   ev.preventDefault();
+//   // извлекаем значение из input (текст)
+//   const currentValue = input.value;
+//   // создаем новый элемент TODO
+//   const newTodoItem = createNewTodo(currentValue);
+//   // добавляем созданный элемент внутрь списка
+//   list.appendChild(newTodoItem);
+// });
+
+
+
+
+
+
+
+
+
 
 // <li className="list-group-item">
 //   <span className="pointer list-group-item-content" data-done="true"> Some todo item </span>
